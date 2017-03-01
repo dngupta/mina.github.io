d9045f9b4dda2836b1c90e796c653a2e3fd37c13
Neha Gupta <dngupta@users.noreply.github.com>
2017-03-01 15:59:34 -0500

    Sidebar loaded
    error on map_filter line 17

--- map_filter.js
+++ map_filter.js
@@ -1,45 +1,8 @@
 $(document).ready(function(){
 
-  renderCategories();
+  renderInstitute();
   initInputs();
 
-  
-var map = L.map('map').setView([23.215757, 80.042289], 5);
-
-L.tileLayer ('https://a.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-	id: 'dngupta.0hm7jnc4',
-    accessToken: 'pk.eyJ1IjoiZG5ndXB0YSIsImEiOiI3ZTUzNDI5MmUyYTBmNDA1NjhmMjViNGI4YTBmNTAxYyJ9.qpu0vHj2vN-pyclLRaj2Uw'  } ).addTo(map);
-
-	//Fetch some data from a GeoJSON file
-	$.getJSON("testData.geojson", function(json) {
-        var place = L.geoJson(json),
-		.on('ready', function(layer)
-            });
-  this.eachLayer(function(marker) {
-          
-            // Bind a popup to each icon based on the same properties
-            // Adds coloumns of extra info to circleMarker popup
-				marker.bindPopup(marker.toGeoJSON().properties.Site + '<br><br>' +
-                marker.toGeoJSON().properties.Year + '<br><br> ' +
-                marker.toGeoJSON().properties.Acc + '<br><br> ' +
-                marker.toGeoJSON().properties.Institute, {autoPanPadding: [5,55]});
-        });
-    )
-            .addTo(map);
-			
-			
-  var FJS = FilterJS("testData.geojson", '#institute', {
-    template: '#institute-template',
-	criteria: [{field: 'Year', ele: '#year_filter', type: 'range'}]
-  });
-  
-  //Add criteria here
-  FJS.addCriteria({field: 'Institute', ele: '#institute_criteria input:checkbox', all: 'all'});
-  FJS.addCriteria({field: 'Ins_ID', ele: '#insID_criteria input:checkbox', all: 'all'});
-
-  window.FJS = FJS;
-});
 
 function initInputs(){
   $("#year_slider").slider({
@@ -62,7 +25,7 @@ function initInputs(){
   });
 }
 
-function renderCategories(){
+function renderInstitute(){
   var institute = ["Survey", "Universities", "State Departments"]; 
 
   var html = $('#institute-template').html();
